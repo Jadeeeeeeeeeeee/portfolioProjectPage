e3037298b3c4bf435b852a0dd268cf0e8731a525
jadeeeeeeeeeeee <yali.shlomi@icloud.com>
2024-03-10 12:54:27 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import SideBar from "./SideBar.jsx"
 import Projects from "./Projects.jsx"
 import quiz from "./assets/quiz.png"
 import todo from "./assets/ToDolist.png"
+import ImageSwiper from './assets/imageSwiper.png'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <div className="ProjectMainDiv">
     <Projects Name="Succses quiz" subText="a quiz that ranks you on how i think you will be in life" imgsrc={quiz} url="https://github.com/Jadeeeeeeeeeeee/succsesQuiz"/>
     <Projects Name="To do list" subText="a simple to do list practice for react" imgsrc={todo} url="https://github.com/Jadeeeeeeeeeeee/ToDoListS"/> 
+    <Projects  Name="image swiper" subText="a simple useState practice for image scrolling with arrows" imgsrc={ImageSwiper} url="https://github.com/Jadeeeeeeeeeeee/image-slider"/> 
      </div>
     </>
   )
